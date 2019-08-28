06e305daa720fcf0ad8e719e0aabfcf071af194b
Lubwama Samuel <lubwamasamuel3@gmail.com>
2019-08-28 19:15:06 +0300

    Setup - Blank Module
    inital Commit
    
    WIP
    
    Delete Non Base files

---
+++ api/src/main/java/org/openmrs/module/patientqueueing/api/dao/PatientQueueingDao.java
@@ -0,0 +1,31 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ * <p>
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.patientqueueing.api.dao;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.db.hibernate.DbSession;
+import org.openmrs.api.db.hibernate.DbSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("patientqueueing.PatientQueueingDao")
+public class PatientQueueingDao {
+	
+	protected final Log log = LogFactory.getLog(getClass());
+	
+	@Autowired
+	DbSessionFactory sessionFactory;
+	
+	public DbSession getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+	
+}
