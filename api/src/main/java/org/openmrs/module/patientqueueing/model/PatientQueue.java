328f227c36c902830348fbd03e2616f5f1c7ff79
dkayiwa <dkayiwa@openmrs.org>
2020-05-27 00:22:27 +0300

    Fixing compiler error

--- api/src/main/java/org/openmrs/module/patientqueueing/model/PatientQueue.java
+++ api/src/main/java/org/openmrs/module/patientqueueing/model/PatientQueue.java
@@ -12,6 +12,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Table;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Column;
 import javax.persistence.ManyToOne;
 import javax.persistence.JoinColumn;
